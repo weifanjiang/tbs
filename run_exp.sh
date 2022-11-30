9dd478c1d7c065ff838b9d36784009bd2ecac215
Weifan Jiang <weifaniang@hotmail.com>
2022-11-29 20:37:33 -0500

    rerun measurements

--- run_exp.sh
+++ run_exp.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
-python3 measurement.py -p examples/cnn-flowers -d cpu -b 64 -t 10
-python3 measurement.py -p examples/cnn-flowers -d cpu -b 128 -t 10
-python3 measurement.py -p examples/cnn-flowers -d cpu -b 256 -t 10
-python3 measurement.py -p examples/cnn-flowers -d cpu -b 512 -t 10
-python3 measurement.py -p examples/cnn-flowers -d cpu -b 1024 -t 10
+python3 measurement.py -p examples/cnn-flowers -d cpu -b 64 -t 10 -c
+python3 measurement.py -p examples/cnn-flowers -d cpu -b 128 -t 10 -c
+python3 measurement.py -p examples/cnn-flowers -d cpu -b 256 -t 10 -c
+python3 measurement.py -p examples/cnn-flowers -d cpu -b 512 -t 10 -c
+python3 measurement.py -p examples/cnn-flowers -d cpu -b 1024 -t 10 -c
 
-python3 measurement.py -p examples/cnn-flowers -d gpu -b 64 -t 10
-python3 measurement.py -p examples/cnn-flowers -d gpu -b 128 -t 10
-python3 measurement.py -p examples/cnn-flowers -d gpu -b 256 -t 10
-python3 measurement.py -p examples/cnn-flowers -d gpu -b 512 -t 10
-python3 measurement.py -p examples/cnn-flowers -d gpu -b 1024 -t 10
+python3 measurement.py -p examples/cnn-flowers -d gpu -b 64 -t 10 -c
+python3 measurement.py -p examples/cnn-flowers -d gpu -b 128 -t 10 -c
+python3 measurement.py -p examples/cnn-flowers -d gpu -b 256 -t 10 -c
+python3 measurement.py -p examples/cnn-flowers -d gpu -b 512 -t 10 -c
+python3 measurement.py -p examples/cnn-flowers -d gpu -b 1024 -t 10 -c
 
-python3 measurement.py -p examples/lm-imdb -d cpu -b 64 -t 10
-python3 measurement.py -p examples/lm-imdb -d cpu -b 128 -t 10
-python3 measurement.py -p examples/lm-imdb -d cpu -b 256 -t 10
-python3 measurement.py -p examples/lm-imdb -d cpu -b 512 -t 10
-python3 measurement.py -p examples/lm-imdb -d cpu -b 1024 -t 10
+python3 measurement.py -p examples/lm-imdb -d cpu -b 64 -t 10 -c
+python3 measurement.py -p examples/lm-imdb -d cpu -b 128 -t 10 -c
+python3 measurement.py -p examples/lm-imdb -d cpu -b 256 -t 10 -c
+python3 measurement.py -p examples/lm-imdb -d cpu -b 512 -t 10 -c
+python3 measurement.py -p examples/lm-imdb -d cpu -b 1024 -t 10 -c
 
-python3 measurement.py -p examples/lm-imdb -d gpu -b 64 -t 10
-python3 measurement.py -p examples/lm-imdb -d gpu -b 128 -t 10
-python3 measurement.py -p examples/lm-imdb -d gpu -b 256 -t 10
-python3 measurement.py -p examples/lm-imdb -d gpu -b 512 -t 10
-python3 measurement.py -p examples/lm-imdb -d gpu -b 1024 -t 10
+python3 measurement.py -p examples/lm-imdb -d gpu -b 64 -t 10 -c
+python3 measurement.py -p examples/lm-imdb -d gpu -b 128 -t 10 -c
+python3 measurement.py -p examples/lm-imdb -d gpu -b 256 -t 10 -c
+python3 measurement.py -p examples/lm-imdb -d gpu -b 512 -t 10 -c
+python3 measurement.py -p examples/lm-imdb -d gpu -b 1024 -t 10 -c
 
-python3 measurement.py -p examples/rnn-mnist -d cpu -b 64 -t 10
-python3 measurement.py -p examples/rnn-mnist -d cpu -b 128 -t 10
-python3 measurement.py -p examples/rnn-mnist -d cpu -b 256 -t 10
-python3 measurement.py -p examples/rnn-mnist -d cpu -b 512 -t 10
-python3 measurement.py -p examples/rnn-mnist -d cpu -b 1024 -t 10
+python3 measurement.py -p examples/rnn-mnist -d cpu -b 64 -t 10 -c
+python3 measurement.py -p examples/rnn-mnist -d cpu -b 128 -t 10 -c
+python3 measurement.py -p examples/rnn-mnist -d cpu -b 256 -t 10 -c
+python3 measurement.py -p examples/rnn-mnist -d cpu -b 512 -t 10 -c
+python3 measurement.py -p examples/rnn-mnist -d cpu -b 1024 -t 10 -c
 
-python3 measurement.py -p examples/rnn-mnist -d gpu -b 64 -t 10
-python3 measurement.py -p examples/rnn-mnist -d gpu -b 128 -t 10
-python3 measurement.py -p examples/rnn-mnist -d gpu -b 256 -t 10
-python3 measurement.py -p examples/rnn-mnist -d gpu -b 512 -t 10
-python3 measurement.py -p examples/rnn-mnist -d gpu -b 1024 -t 10
+python3 measurement.py -p examples/rnn-mnist -d gpu -b 64 -t 10 -c
+python3 measurement.py -p examples/rnn-mnist -d gpu -b 128 -t 10 -c
+python3 measurement.py -p examples/rnn-mnist -d gpu -b 256 -t 10 -c
+python3 measurement.py -p examples/rnn-mnist -d gpu -b 512 -t 10 -c
+python3 measurement.py -p examples/rnn-mnist -d gpu -b 1024 -t 10 -c
